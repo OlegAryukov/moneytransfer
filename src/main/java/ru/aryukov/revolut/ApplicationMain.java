256a1078457a0e43067545f449a8faafb539c0c4
Oleg.Aryukov <oleg.aryukov@billing.ru>
2018-10-23 20:04:54 +0300

    some cleaning

--- src/main/java/ru/aryukov/revolut/ApplicationMain.java
+++ src/main/java/ru/aryukov/revolut/ApplicationMain.java
@@ -6,24 +6,20 @@ import org.h2.tools.Server;
 import ru.aryukov.revolut.config.Configuration;
 import ru.aryukov.revolut.dto.response.ErrorResponse;
 import ru.aryukov.revolut.utils.JsonUtil;
-import ru.aryukov.revolut.utils.SparkUtils;
 
 import java.sql.SQLException;
 
 import static spark.Spark.exception;
-import static spark.Spark.get;
 
 public class ApplicationMain {
     public static void main(String[] args) throws SQLException {
+        Logger logger = Logger.getLogger(ApplicationMain.class);
+
         //Код для возможности контроля базы данных в консоли
         Server webServer = Server.createWebServer("-webPort", "8082", "-tcpAllowOthers").start();
         Server server = Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
         //
         Configuration.initialize();
-
-        Logger logger = Logger.getLogger(ApplicationMain.class);
-        SparkUtils.createServerWithRequestLog(logger);
-        get("/hello", (request, response) -> "world");
 
         exception(Exception.class, (e, req, res) -> {
             logger.error(String.format("%s : Got an exception for request : %s  ", e.getLocalizedMessage(), req.url()));
